0b026eb6779883c344fb17808ccb1a1e41f756fd
Alexey <33495117+Avigdor-Kolonimus@users.noreply.github.com>
2026-04-09 20:18:40 +0900

    prctc-4: assignment 1 and 2

---
+++ coderun_yandex/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	cy "coderun_yandex/assignments"
+)
+
+func main() {
+	fmt.Println("SharingMushrooms - assignment 1")
+	// cy.SharingMushrooms()
+
+	fmt.Println("MomsReands - assignment 2")
+	cy.MomsReands()
+}
